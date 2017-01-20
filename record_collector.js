db0213c352e1462d8fe580957e7a0843fcb706d0
Elgrogz <gregorgilchrist@gmail.com>
2017-01-20 19:45:55 +0000

    recordCollector name, funds and add tested

--- record_collector.js
+++ record_collector.js
@@ -1,1 +1,19 @@
+var RecordCollector = function(name, funds) {
+  this.name = name;
+  this.funds = funds;
+  this.recordCollection = [];
+}
+
+RecordCollector.prototype = {
+  addRecord: function(record) {
+    this.recordCollection.push(record);
+  },
+  buyRecord: function(record, shop) {
+
+  },
+  // sellRecord: function() {
+
+  // }
+}
+
 module.exports = RecordCollector;
--- record_shop_specs/record_collector_spec.js
+++ record_shop_specs/record_collector_spec.js
@@ -8,15 +8,34 @@ describe('Record', function() {
   var collector;
   var record1;
   var record2;
+  var record3;
   var recordShop;
 
   beforeEach(function() {
     collector = new RecordCollector("Gregor", 12);
     record1 = new Record("Californication", "Red Hot Chili Peppers", 10);
     record2 = new Record("Good Kid, Mad City", "Kendrick Lamar", 15);
+    record3 = new Record("A.M", "Arctic Monkeys", 20);
     recordShop = new RecordShop("GG's Records", "Dunfermline");
-    recordShop.add(record1);
-    recordShop.add(record2);
+    recordShop.addRecord(record1);
+    recordShop.addRecord(record2);
   });
+
+  it('should have a name', function() {
+    assert.equal("Gregor", collector.name);
+  });
+
+  it('should have funds', function() {
+    assert.equal(12, collector.funds);
+  });
+
+  it('can add record', function() {
+    collector.addRecord(record3);
+    assert.deepEqual([record3], collector.recordCollection);
+  });
+
+  it('can buy record', function() {
+    
+  })
 
 })
