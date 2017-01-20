ab88355b6b7fe10ec322372f03421312b47136db
Elgrogz <gregorgilchrist@gmail.com>
2017-01-20 12:02:25 +0000

    title tested

---
+++ record.js
@@ -0,0 +1,7 @@
+var Record = function(title, artist, cost) {
+  this.title = title;
+  this.artist = artist;
+  this.cost = cost;
+}
+
+module.exports = Record;
---
+++ record_shop_specs/record_spec.js
@@ -0,0 +1,21 @@
+var Record = require('../record');
+var assert = require('assert');
+
+describe('Record', function() {
+
+  var record1;
+  var record2;
+
+  beforeEach(function() {
+    record1 = new Record("Californication", "Red Hot Chili Peppers", 10);
+    record2 = new Record("Good Kid, Mad City", "Kendrick Lamar", 15);
+  });
+
+  it('should have a title', function() {
+    assert.equal("Californication", record1.title);
+  });
+
+
+
+
+})
